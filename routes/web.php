124e83d9fc9c940b90bb78bae0b07961b9976f1e
Indriana Noviyanti <indriana@student.telkomuniversity.ac.id>
2024-06-01 13:56:01 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Lat1Controller;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/lat1', 'App\Http\Controllers\Lat1Controller@index');
+Route::get('/lat1', [Lat1Controller::class, 'index']);
